fa5f6cc2606f2d3e4887889d4901aabfc4c9b69f
zhusaidong <635925926@qq.com>
2016-10-12 12:31:18 +0800

    add demo file and screenshot files

--- demo.php
+++ demo.php
@@ -1,36 +1,87 @@
 <?php
 require_once("./common.php");
-
 $savePath         = './DownLoadFont/';
 $saveFontListFile = './FontList.txt';
 $fontList         = GetFontListArray($saveFontListFile,$savePath);
-
-$baseUrl = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].dirname($_SERVER["REQUEST_URI"]).'/';
-
-echo '<table border="1" cellspacing="0">';
-echo '<tr>';
-echo '<td>';
-echo '字体名称';
-echo '</td>';
-echo '<td>';
-echo 'image版';
-echo '</td>';
-echo '<td>';
-echo 'html版';
-echo '</td>';
-echo '</tr>';
-foreach($fontList as $key => $value)
-{
-	echo '<tr>';
-	echo '<td>';
-	echo $key;
-	echo '</td>';
-	echo '<td>';
-	echo '<img src="image.php?name='.str_replace('字体','',$key).'&font='.$key.'"/>';
-	echo '</td>';
-	echo '<td>';
-	echo GetData($baseUrl.'/html.php?name='.str_replace('字体','',$key).'&font='.$key);
-	echo '</td>';
-	echo '</tr>';
-}
-echo '</table>';
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+	<head>
+		<title>
+			PHPArtSign demo
+		</title>
+		<meta name="author" content="zsdroid">
+		<style>
+			body
+			{
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+		<h2>
+			PHPArtSign demo
+		</h2>
+		<div class="html">
+			<h3>
+				html版PHPArtSign demo
+			</h3>
+			<form action="./html.php">
+				<label for="">
+					字体库:
+					<select name="font">
+						<option value="">
+							选择字体库
+						</option>
+						<?php
+						foreach($fontList as $key =>$value):?>
+						<option value="<?=$key?>">
+							<?=$key?>
+						</option>
+						<?php endforeach;?>
+					</select>
+				</label>
+				<label for="">
+					文本:
+					<input type="text" name="name" value="" />
+				</label>
+				<label for="">
+					<input type="submit" value="提交" />
+				</label>
+			</form>
+		</div>
+		<div class="image">
+			<h3>
+				image版PHPArtSign demo
+			</h3>
+			<form action="./image.php">
+				<label for="">
+					字体库:
+					<select name="font">
+						<option value="">
+							选择字体库
+						</option>
+						<?php
+						foreach($fontList as $key =>$value):?>
+						<option value="<?=$key?>">
+							<?=$key?>
+						</option>
+						<?php endforeach;?>
+					</select>
+				</label>
+				<label for="">
+					文本:
+					<input type="text" name="name" value="" />
+				</label>
+				<label for="">
+					<input type="submit" value="提交" />
+				</label>
+			</form>
+		</div>
+		<div>
+			<a href="./list.php">
+				点击查看字体效果列表
+			</a>
+		</div>
+	</body>
+</html>
